73a5012f413e6f275ca279b6032f9a055a1b861e
Ganesh Patil <ganeshpatil.1997@gmail.com>
2021-12-15 00:06:19 +0000

    database modified

---
+++ includes/config.inc.php
@@ -0,0 +1,13 @@
+<?php
+  session_start();
+  $servername = "cppdb.cmetaa25g7bh.us-east-1.rds.amazonaws.com"; //change this  accordingly
+  $dBUsername = "root";
+  $dBPassword = "12345678";
+  $dBName = "hostel_management_system";
+ // session_start();
+  $conn=mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
+
+  if (!$conn) {
+    die("Connection Failed: ".mysqli_connect_error());
+  }
+?>

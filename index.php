94af5f84a3db2c24d649c505c5f22260979f4aee
Ganesh <75245204+ganeshpatil97@users.noreply.github.com>
2021-12-15 21:54:45 +0000

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 //Set smtp encryption type (ssl/tls)
 	$mail->SMTPSecure = "tls";
 //Port to connect smtp
-	$mail->Port = "587";
+	$mail->Port = "465";
 //Set gmail username
 	$mail->Username = "test.nileshwari@gmail.com";
 //Set gmail password

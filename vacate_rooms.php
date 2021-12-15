715d7e88616f9b1474540f200935c9c8929af480
Ganesh Patil <ganeshpatil.1997@gmail.com>
2021-12-15 13:40:46 +0000

    changes in vacate rooms

--- vacate_rooms.php
+++ vacate_rooms.php
@@ -47,7 +47,7 @@
 		<div class="container agile-banner_nav">
 			<nav class="navbar navbar-expand-lg navbar-light bg-light">
 				
-				<h1><a class="navbar-brand" href="home.php">In <span class="display"> Trend</span></a></h1>
+				<h1><a class="navbar-brand" href="home.php">NCI</a></h1>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 				</button>
@@ -194,7 +194,7 @@ if(isset($_POST['submit'])){
 <footer class="py-5">
 	<div class="container py-md-5">
 		<div class="footer-logo mb-5 text-center">
-			<a class="navbar-brand" href="index.html">In <span class="display"> Trend</span></a>
+			<a class="navbar-brand" href="index.html">NCI <span class="display"> DUBLIN</span></a>
 		</div>
 		<div class="footer-grid">
 			<div class="social mb-4 text-center">
